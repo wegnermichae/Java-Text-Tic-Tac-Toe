51b6d9f919e94c2ae6389dcbef62db50575e9e98
wegnermichae <wegnermichael14@gmail.com>
2024-03-10 12:24:23 -0500

    Added Try Catch for incorrect data-type inputs

--- Tic Tac Toe/src/main/java/org/example/Main.java
+++ Tic Tac Toe/src/main/java/org/example/Main.java
@@ -5,11 +5,12 @@ Author - Michael Wegner
  */
 
 package org.example;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     static boolean play;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InputMismatchException {
         Player player = new Player();
         Board board = new Board();
         int count = 0;
@@ -17,26 +18,30 @@ public class Main {
         while(!play){
             System.out.println("Enter location of move: (1-9)");
             board.print_board();
-            Scanner in = new Scanner(System.in);
-            int move = in.nextInt();
-            Game game = new Game(move);
-            if(count % 2 != 0){
-                if(board.check_fill(game.row_index(), game.col_index())){
-                    board.modify_board(game.row_index(), game.col_index(), player.getPlayer_one());
-                    count++;
+            try{
+                Scanner in = new Scanner(System.in);
+                int move = in.nextInt();
+                Game game = new Game(move);
+                if(count % 2 != 0){
+                    if(board.check_fill(game.row_index(), game.col_index())){
+                        board.modify_board(game.row_index(), game.col_index(), player.getPlayer_one());
+                        count++;
+                    }else{
+                        System.out.println("The location you selected is filled already, please try again");
+                    }
                 }else{
-                    System.out.println("The location you selected is filled already, please try again");
+                    if(board.check_fill(game.row_index(), game.col_index())){
+                        board.modify_board(game.row_index(), game.col_index(), player.getPlayer_two());
+                        count++;
+                    }else{
+                        System.out.println("The location you selected is filled already, please try again");
+                    }
                 }
-            }else{
-                if(board.check_fill(game.row_index(), game.col_index())){
-                    board.modify_board(game.row_index(), game.col_index(), player.getPlayer_two());
-                    count++;
-                }else{
-                    System.out.println("The location you selected is filled already, please try again");
-                }
+                board.print_board();
+                play = board.win();
+            }catch (InputMismatchException e){
+                System.out.println("Please enter an Integer (1-9)");
             }
-            board.print_board();
-            play = board.win();
         }
         System.out.println("Congrats you win!");
     }
